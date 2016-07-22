4da05b988626d2853a7dd96d42a4ebe3ae7c5140
MacKentoch <erwan.datin@gmail.com>
2016-07-22 06:14:24 +0200

    tests

--- test/containers/home/Home.spec.js
+++ test/containers/home/Home.spec.js
@@ -3,24 +3,24 @@ import {expect}             from 'chai';
 import {shallow}            from 'enzyme';
 import configureMockStore   from 'redux-mock-store';
 import thunk                from 'redux-thunk';
-import AboutConnected       from '../../../src/app/containers/about/About';
+import HomeConnected       from '../../../src/app/containers/home/Home';
 
 const mockStore = configureMockStore([ thunk ]);
 const storeStateMock = {
   views: {
-    currentView: 'about'
+    currentView: 'home'
   }
 };
 
 let store;
 let wrapper;
-describe('AboutConnected (About container) ', () => {
+describe('HomeConnected (Home container) ', () => {
   beforeEach(() => {
     store = mockStore(storeStateMock);
-    wrapper = shallow(<AboutConnected store={store} />).shallow();
+    wrapper = shallow(<HomeConnected store={store} />).shallow();
   });
 
   it('should render container', () => {
-    expect(wrapper.containsMatchingElement(<h1>About</h1>));
+    expect(wrapper.containsMatchingElement(<h1>Full ES2015 ReactJS + Bootstrap</h1>));
   });
 });
