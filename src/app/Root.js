58379a3b04843051c0f38cbae6a87fae7dcea8d9
MacKentoch <erwan.datin@gmail.com>
2017-12-07 14:29:36 +0100

    fix - react-router-redux is now 5.x

--- src/app/Root.js
+++ src/app/Root.js
@@ -5,10 +5,13 @@ import React, {
   Component
 }                               from 'react';
 import {
-  Router,
+  // Router, // using now ConnectedRouter from react-router-redux v5.x (the only one compatible react-router 4)
   Switch,
   Route
 }                               from 'react-router-dom';
+import { ConnectedRouter }      from 'react-router-redux';
+import { Provider }             from 'react-redux';
+import configureStore           from './redux/store/configureStore';
 import { history }              from './redux/store/configureStore';
 import App                      from './containers/app';
 import ScrollTop                from './components/scrollToTop/ScrollToTop';
@@ -22,22 +25,26 @@ type Props = any;
 type State = any;
 // #endregion
 
+const store = configureStore();
+
 class Root extends Component<Props, State> {
   render() {
     return (
-      <div>
-        <Router history={history}>
-          <ScrollTop>
-            <Switch>
-              <Route exact path="/login" component={Login} />
-              <App />
-              {/* logout: just redirects to login (App will take care of removing the token) */}
-              <LogoutRoute path="/logout" />
-              <Route component={PageNotFound} />
-            </Switch>
-          </ScrollTop>
-        </Router>
-      </div>
+      <Provider store={store}>
+        <div>
+          <ConnectedRouter history={history}>
+            <ScrollTop>
+              <Switch>
+                <Route exact path="/login" component={Login} />
+                <App />
+                {/* logout: just redirects to login (App will take care of removing the token) */}
+                <LogoutRoute path="/logout" />
+                <Route component={PageNotFound} />
+              </Switch>
+            </ScrollTop>
+          </ConnectedRouter>
+        </div>
+      </Provider>
     );
   }
 }
--- src/app/index.js
+++ src/app/index.js
@@ -4,9 +4,7 @@ import React                from 'react';
 import {render}             from 'react-dom';
 import injectTpEventPlugin  from 'react-tap-event-plugin';
 import { AppContainer }     from 'react-hot-loader';
-import { Provider }         from 'react-redux';
 import smoothScrollPolyfill from 'smoothscroll-polyfill';
-import configureStore       from './redux/store/configureStore';
 import 'animate.css';
 import 'jquery';
 import 'font-awesome/css/font-awesome.min.css';
@@ -25,16 +23,13 @@ const BootstrapedElement    = document.getElementById(ELEMENT_TO_BOOTSTRAP);
 
 injectTpEventPlugin();
 
-const store = configureStore();
 
 const renderApp = RootComponent => {
   render(
     <AppContainer
       warnings={false}
     >
-      <Provider store={store}>
-        <RootComponent />
-     </Provider>
+      <RootComponent />
     </AppContainer>,
     BootstrapedElement
   );
