5fdd65a89b63817c63830f9686a3b65f2fa2a5de
MacKentoch <erwan.datin@gmail.com>
2018-05-16 06:30:42 +0200

    tests

---
+++ src/front/components/navigation/__tests__/NavigationBar.test.js
@@ -0,0 +1,38 @@
+// @flow
+
+// #region imports
+import React from 'react';
+import renderer from 'react-test-renderer'; // needed both for snpashot testing but also to prevent errors from enzyme
+import { MemoryRouter } from 'react-router';
+import NavigationBar from '../NavigationBar';
+// #endregion
+
+describe('NavigationBar component', () => {
+  it('renders as expected', () => {
+    const props = {
+      brand: 'test',
+      handleLeftNavItemClick: () => {},
+      handleRightNavItemClick: () => {},
+      navModel: {
+        leftLinks: [
+          { link: '1', label: 'view 1', viewName: 'view 1' },
+          { link: '2', label: 'view 2', viewName: 'view 2' },
+        ],
+        rightLinks: [
+          { link: '1', label: 'view 1', viewName: 'view 1' },
+          { link: '2', label: 'view 2', viewName: 'view 2' },
+        ],
+      },
+    };
+    const component = renderer
+      .create(
+        <div>
+          <MemoryRouter>
+            <NavigationBar {...props} />
+          </MemoryRouter>
+        </div>,
+      )
+      .toJSON();
+    expect(component).toMatchSnapshot();
+  });
+});
