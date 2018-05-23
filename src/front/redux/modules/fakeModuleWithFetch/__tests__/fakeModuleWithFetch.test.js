a255daf74ce6b5fd589005ccd9f6b7c13e10da41
MacKentoch <erwan.datin@gmail.com>
2018-05-23 06:33:55 +0200

    redux module

---
+++ src/front/redux/modules/fakeModuleWithFetch/__tests__/fakeModuleWithFetch.test.js
@@ -0,0 +1,11 @@
+// @flow
+
+// #region imports
+import { fakeFetch } from '../index';
+// #endregion
+
+describe('fakeFetch action creators', () => {
+  it('should fetch ', () => {
+    //
+  });
+});
