a96c770e78467350f76e3f51279bcb3f4c93cf72
MacKentoch <erwan.datin@gmail.com>
2018-05-25 06:48:06 +0200

    tests

--- src/front/redux/modules/fakeModuleWithFetch/__tests__/fakeModuleWithFetch.test.js
+++ src/front/redux/modules/fakeModuleWithFetch/__tests__/fakeModuleWithFetch.test.js
@@ -1,11 +1,39 @@
 // @flow
 
 // #region imports
-import { fakeFetch } from '../index';
+import { fakeFetchIfNeeded } from '../index';
 // #endregion
 
-describe('fakeFetch action creators', () => {
-  it('should fetch ', () => {
-    //
+// #region jest mocks (JSON files)
+jest.mock(
+  '../../mock/fakeAPI.json',
+  () => [
+    {
+      id: 1,
+      label: 'item 1',
+    },
+    {
+      id: 2,
+      label: 'item 2',
+    },
+  ],
+  { virtual: true },
+);
+
+jest.mock('../../../services/API/fetchTools', () => ({
+  getLocationOrigin: 'http://localhost',
+}));
+// #endregion
+
+describe('fakeFetchIfNeeded action creators', () => {
+  it('should fetch', async () => {
+    expect.assertions(1);
+
+    try {
+      const returnValue = await fakeFetchIfNeeded();
+      expect(returnValue).toBeDefined();
+    } catch (error) {
+      expect(error).toBeDefined();
+    }
   });
 });
