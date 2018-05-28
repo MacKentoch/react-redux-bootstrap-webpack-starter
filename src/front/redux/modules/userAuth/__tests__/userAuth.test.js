61b2d288d69974d57f37bce18a33fc4ac539bceb
MacKentoch <erwan.datin@gmail.com>
2018-05-28 06:32:13 +0200

    tests

--- src/front/redux/modules/userAuth/__tests__/userAuth.test.js
+++ src/front/redux/modules/userAuth/__tests__/userAuth.test.js
@@ -1,7 +1,30 @@
 // @flow
 
 // #region imports
+import configureMockStore from 'redux-mock-store';
+import thunk from 'redux-thunk';
+import fetchMiddleware from '../../../middleware/fetchMiddleware';
 import { disconnectUser } from '../index';
+// #endregion
+
+// #region constants
+const middlewares = [thunk, fetchMiddleware];
+const mockStore = configureMockStore(middlewares);
+const initialState = {
+  // actions details
+  isFetching: false,
+  isLogging: false,
+  time: '',
+
+  // userInfos
+  id: '',
+  login: '',
+  firstname: '',
+  lastname: '',
+
+  token: null,
+  isAuthenticated: false, // authentication status (token based auth)
+};
 // #endregion
 
 // #region jest mocks (JSON files)
@@ -11,6 +34,11 @@ jest.mock('../../../../services/API/fetchTools', () => ({
 // #endregion
 
 describe('userAuth action creators', () => {
+  let store = null;
+
+  beforeEach(() => {
+    store = mockStore(initialState);
+  });
   it('disconnectUser should return valid action', async () => {
     const expectedAction = { type: 'DISCONNECT_USER' };
 
