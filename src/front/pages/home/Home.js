557e197431b30ede316916c38c5bbe63c7ea9f7b
MacKentoch <erwan.datin@gmail.com>
2018-08-13 16:56:56 +0200

    migration

--- src/front/pages/home/Home.js
+++ src/front/pages/home/Home.js
@@ -8,7 +8,7 @@ import {
   type RouterHistory,
 } from 'react-router-dom';
 import { Link } from 'react-router-dom';
-import Jumbotron from '../../components/jumbotron/Jumbotron';
+import Jumbotron from 'reactstrap/lib/Jumbotron';
 import HomeInfo from './styled/HomeInfo';
 import MainTitle from './styled/MainTitle';
 import LightNote from './styled/LightNote';
@@ -34,16 +34,19 @@ class Home extends PureComponent<Props, State> {
       <div>
         <Jumbotron>
           <HomeInfo>
-            <MainTitle>ReactJS 16.3+ Redux Bootstrap</MainTitle>
+            <MainTitle>ReactJS 16.3+ Bootstrap 4</MainTitle>
             <h2>
-              with Hot Reload (<i>react-hot-loader 4+</i>)!!!
+              with Hot Reload (<i>react-hot-loader 4+</i>
+              )!!!
             </h2>
             <h2>and React Router v4</h2>
             <h2>and webpack 4.x</h2>
             <h2>
-              and styled-components (<LightNote>
+              and styled-components (
+              <LightNote>
                 so keep using SCSS like styles and benefit performant css-in-js
-              </LightNote>)
+              </LightNote>
+              )
             </h2>
             <h1>Starter</h1>
             <p>
