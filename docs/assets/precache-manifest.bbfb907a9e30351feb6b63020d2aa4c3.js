f20820d52e0688f6a5fb3e4ba0a81ca3f6ae20fb
MacKentoch <erwan.datin@gmail.com>
2018-08-21 06:37:32 +0200

    fix statics

---
+++ docs/assets/precache-manifest.bbfb907a9e30351feb6b63020d2aa4c3.js
@@ -0,0 +1,37 @@
+self.__precacheManifest = [
+  {
+    "url": "/assets/5.369f4429cf767c12bf4c.js"
+  },
+  {
+    "url": "/assets/vendors.369f4429cf767c12bf4c.js.gz"
+  },
+  {
+    "url": "/assets/vendors.369f4429cf767c12bf4c.js"
+  },
+  {
+    "revision": "98ee0f9b24d8396b668f6c70aa3a52d4",
+    "url": "/assets/modernizr-bundle.js"
+  },
+  {
+    "url": "/assets/app.369f4429cf767c12bf4c.js.gz"
+  },
+  {
+    "url": "/assets/app.369f4429cf767c12bf4c.js"
+  },
+  {
+    "url": "/assets/6.369f4429cf767c12bf4c.js"
+  },
+  {
+    "url": "/assets/4.369f4429cf767c12bf4c.js"
+  },
+  {
+    "url": "/assets/3.369f4429cf767c12bf4c.js"
+  },
+  {
+    "url": "/assets/2.369f4429cf767c12bf4c.js"
+  },
+  {
+    "revision": "f4c3a5763068f1aa34a0e9411e436142",
+    "url": "/assets/../index.html"
+  }
+];
