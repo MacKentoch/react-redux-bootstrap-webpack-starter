a264d43b8b7e603f0963032e30e385ef24ffea5a
MacKentoch <erwan.datin@gmail.com>
2019-02-25 06:02:20 +0100

    hooks

--- src/front/pages/login/Login.js
+++ src/front/pages/login/Login.js
@@ -1,7 +1,7 @@
 // @flow
 
 // #region imports
-import React, { PureComponent } from 'react';
+import React, { useState, useEffect } from 'react';
 import {
   type Match,
   type Location,
@@ -26,146 +26,45 @@ export type Props = {
   isFetching: boolean,
   isLogging: boolean,
 } & UserAuthActions;
-
-export type State = {
-  email: string,
-  password: string,
-};
 // #endregion
 
-class Login extends PureComponent<Props, State> {
-  static defaultProps = {
-    isFetching: false,
-    isLogging: false,
-  };
+function Login({
+  isLogging,
+  isFetching,
+  disconnectUser,
+  logUserIfNeeded,
+  history,
+}: Props) {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
 
-  state = {
-    email: '',
-    password: '',
-  };
-
-  // #region lifecycle methods
-  componentDidMount() {
-    const { disconnectUser } = this.props;
-    disconnectUser(); // diconnect user: remove token and user info
-  }
-
-  render() {
-    const { email, password } = this.state;
-    const { isLogging } = this.props;
-
-    return (
-      <div className="content">
-        <Row>
-          <Col md={{ size: 4, offset: 4 }} xs={{ size: 10, offset: 1 }}>
-            <form className="form-horizontal" noValidate>
-              <fieldset>
-                <legend>Login</legend>
-
-                <div className="form-group">
-                  <label
-                    htmlFor="inputEmail"
-                    className="col-lg-2 control-label"
-                  >
-                    Email
-                  </label>
-                  <Col lg={12}>
-                    <input
-                      type="text"
-                      className="form-control"
-                      id="inputEmail"
-                      placeholder="Email"
-                      autoComplete="username email"
-                      value={email}
-                      onChange={this.handlesOnEmailChange}
-                    />
-                  </Col>
-                </div>
-
-                <div className="form-group">
-                  <label
-                    htmlFor="inputPassword"
-                    className="col-lg-2 control-label"
-                  >
-                    Password
-                  </label>
-                  <Col lg={12}>
-                    <input
-                      type="password"
-                      className="form-control"
-                      id="inputPassword"
-                      placeholder="Password"
-                      autoComplete="current-password"
-                      value={password}
-                      onChange={this.handlesOnPasswordChange}
-                    />
-                  </Col>
-                </div>
-
-                <div className="form-group">
-                  <Col lg={{ size: 12 }}>
-                    <Button
-                      className="login-button btn-block"
-                      color="primary"
-                      disabled={isLogging}
-                      onClick={this.handlesOnLogin}
-                    >
-                      {isLogging ? (
-                        <span>
-                          login in... &nbsp;
-                          <i className="fa fa-spinner fa-pulse fa-fw" />
-                        </span>
-                      ) : (
-                        <span>Login</span>
-                      )}
-                    </Button>
-                  </Col>
-                </div>
-              </fieldset>
-            </form>
-          </Col>
-        </Row>
-        <Row>
-          <Col md={{ size: 4, offset: 4 }} xs={{ size: 10, offset: 1 }}>
-            <div className="pull-right">
-              <Button className="btn-block" onClick={this.goHome}>
-                back to home
-              </Button>
-            </div>
-          </Col>
-        </Row>
-      </div>
-    );
-  }
-  // #endregion
+  useEffect(
+    () => {
+      // on mount disconnect (clear previous user auth and info):
+      disconnectUser();
+    },
+    () => {},
+  );
 
   // #region form inputs change callbacks
-  handlesOnEmailChange = (event: SyntheticEvent<>) => {
+  const handlesOnEmailChange = (event: SyntheticEvent<>) => {
     if (event) {
       event.preventDefault();
-      // should add some validator before setState in real use cases
-      this.setState({ email: event.target.value.trim() });
+      setEmail(event.target.value.trim());
     }
   };
 
-  handlesOnPasswordChange = (event: SyntheticEvent<>) => {
+  const handlesOnPasswordChange = (event: SyntheticEvent<>) => {
     if (event) {
       event.preventDefault();
-      // should add some validator before setState in real use cases
-      this.setState({ password: event.target.value.trim() });
+      setPassword(event.target.value.trim());
     }
   };
   // #endregion
 
   // #region on login button click callback
-  handlesOnLogin = async (event: SyntheticEvent<>) => {
-    if (event) {
-      event.preventDefault();
-    }
-
-    const { history, logUserIfNeeded } = this.props;
-
-    const { email, password } = this.state;
+  const handlesOnLogin = async (event: SyntheticEvent<>) => {
+    event && event.preventDefault();
 
     const userLogin = {
       login: email,
@@ -191,16 +90,97 @@ class Login extends PureComponent<Props, State> {
   // #endregion
 
   // #region on go back home button click callback
-  goHome = (event: SyntheticEvent<>) => {
-    if (event) {
-      event.preventDefault();
-    }
-
-    const { history } = this.props;
-
+  const goHome = (event: SyntheticEvent<>) => {
+    event && event.preventDefault();
     history.push({ pathname: '/' });
   };
   // #endregion
+
+  return (
+    <div className="content">
+      <Row>
+        <Col md={{ size: 4, offset: 4 }} xs={{ size: 10, offset: 1 }}>
+          <form className="form-horizontal" noValidate>
+            <fieldset>
+              <legend>Login</legend>
+              <div className="form-group">
+                <label htmlFor="inputEmail" className="col-lg-2 control-label">
+                  Email
+                </label>
+                <Col lg={12}>
+                  <input
+                    type="text"
+                    className="form-control"
+                    id="inputEmail"
+                    placeholder="Email"
+                    autoComplete="username email"
+                    value={email}
+                    onChange={handlesOnEmailChange}
+                  />
+                </Col>
+              </div>
+
+              <div className="form-group">
+                <label
+                  htmlFor="inputPassword"
+                  className="col-lg-2 control-label"
+                >
+                  Password
+                </label>
+                <Col lg={12}>
+                  <input
+                    type="password"
+                    className="form-control"
+                    id="inputPassword"
+                    placeholder="Password"
+                    autoComplete="current-password"
+                    value={password}
+                    onChange={handlesOnPasswordChange}
+                  />
+                </Col>
+              </div>
+
+              <div className="form-group">
+                <Col lg={{ size: 12 }}>
+                  <Button
+                    className="login-button btn-block"
+                    color="primary"
+                    disabled={isLogging}
+                    onClick={handlesOnLogin}
+                  >
+                    {isLogging ? (
+                      <span>
+                        login in... &nbsp;
+                        <i className="fa fa-spinner fa-pulse fa-fw" />
+                      </span>
+                    ) : (
+                      <span>Login</span>
+                    )}
+                  </Button>
+                </Col>
+              </div>
+            </fieldset>
+          </form>
+        </Col>
+      </Row>
+      <Row>
+        <Col md={{ size: 4, offset: 4 }} xs={{ size: 10, offset: 1 }}>
+          <div className="pull-right">
+            <Button className="btn-block" onClick={goHome}>
+              back to home
+            </Button>
+          </div>
+        </Col>
+      </Row>
+    </div>
+  );
 }
+
+Login.defaultProps = {
+  isFetching: false,
+  isLogging: false,
+};
+
+Login.displayName = 'Login';
 
 export default Login;
