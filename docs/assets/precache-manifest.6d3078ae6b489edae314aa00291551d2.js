6cb038213dea4f292a1b8b63d124f6026156a92d
MacKentoch <erwan.datin@gmail.com>
2019-07-14 08:31:53 +0200

    prod bundled

---
+++ docs/assets/precache-manifest.6d3078ae6b489edae314aa00291551d2.js
@@ -0,0 +1,37 @@
+self.__precacheManifest = [
+  {
+    "url": "/assets/5.7669af16a2c9df7f93e4.js"
+  },
+  {
+    "url": "/assets/vendors.7669af16a2c9df7f93e4.js.gz"
+  },
+  {
+    "url": "/assets/vendors.7669af16a2c9df7f93e4.js"
+  },
+  {
+    "revision": "98ee0f9b24d8396b668f6c70aa3a52d4",
+    "url": "/assets/modernizr-bundle.js"
+  },
+  {
+    "url": "/assets/app.7669af16a2c9df7f93e4.js.gz"
+  },
+  {
+    "url": "/assets/app.7669af16a2c9df7f93e4.js"
+  },
+  {
+    "url": "/assets/6.7669af16a2c9df7f93e4.js"
+  },
+  {
+    "url": "/assets/4.7669af16a2c9df7f93e4.js"
+  },
+  {
+    "url": "/assets/3.7669af16a2c9df7f93e4.js"
+  },
+  {
+    "url": "/assets/2.7669af16a2c9df7f93e4.js"
+  },
+  {
+    "revision": "9435645da8d54bc00b375d7c62223a21",
+    "url": "/assets/../index.html"
+  }
+];
