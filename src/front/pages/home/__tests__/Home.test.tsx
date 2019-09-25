6e0de9611a90906463d158d1469ab4958e8efd90
MacKentoch <erwan.datin@gmail.com>
2019-09-25 05:06:53 +0200

    migration

--- src/front/pages/home/__tests__/Home.test.tsx
+++ src/front/pages/home/__tests__/Home.test.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import { shallow } from 'enzyme';
 import { MemoryRouter } from 'react-router';
 import { Route } from 'react-router-dom';
-import Home from '../Index';
+import Home from '../index';
 
 describe('Home page', () => {
   it('renders as expected', () => {
