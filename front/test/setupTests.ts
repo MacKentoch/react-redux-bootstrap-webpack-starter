ed2089f012c5f54559136487ce16b1a770ac3320
MacKentoch <erwan.datin@gmail.com>
2019-10-30 06:06:24 +0100

    setupJest fix warning

--- front/test/setupTests.ts
+++ front/test/setupTests.ts
@@ -1,10 +1,15 @@
 // to avoid: Warning: React depends on requestAnimationFrame. Make sure that you load a polyfill in older browsers. http://fb.me/react-polyfills
+// @ts-ignore
 require('raf/polyfill');
+// @ts-ignore
 require('jest-localstorage-mock');
+// @ts-ignore
 require('core-js/stable');
+// @ts-ignore
 require('regenerator-runtime/runtime');
-
+// @ts-ignore
 const Enzyme = require('enzyme');
+// @ts-ignore
 const EnzymeAdapter = require('enzyme-adapter-react-16');
 
 // Setup enzyme's react adapter
