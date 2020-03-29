8e4fd364d5ee83a7a58b988c94bff219eeb5ab61
MacKentoch <erwan.datin@gmail.com>
2020-03-29 08:58:04 +0200

    tests migration

--- front/src/layout/mainLayout/__tests__/MainLayout.test.tsx
+++ front/src/layout/mainLayout/__tests__/MainLayout.test.tsx
@@ -1,17 +1,43 @@
 import React from 'react';
+import { render } from '@testing-library/react';
 import { MemoryRouter } from 'react-router';
-import { shallow } from 'enzyme';
-import MainLayout from '../index';
+import MainLayout from '../MainLayout';
 
-describe('MainLayout', () => {
+jest.mock('../../navigation');
+jest.mock('../../components/backToTop/BackToTop');
+
+describe('MainLayout component', () => {
+  let rootElement: any = null;
+
+  beforeEach(() => {
+    rootElement = document.createElement('div');
+    document.body.appendChild(rootElement);
+
+    jest.restoreAllMocks();
+  });
+
+  afterEach(() => {
+    rootElement && document.body.removeChild(rootElement);
+    rootElement = null;
+  });
+
   it('renders as expected', () => {
-    const component = shallow(
+    const NavigationBar = require('../../navigation');
+    const BackToTop = require('../../components/backToTop/BackToTop');
+    // React component is default exported:
+    NavigationBar.default.mockImplementationOnce(() => <span>navbar</span>);
+    BackToTop.default.mockImplementationOnce(() => (
+      <span>backtotopbutton</span>
+    ));
+
+    const { container } = render(
       <MemoryRouter>
         <MainLayout>
-          <p>a child</p>
+          <p>children here</p>
         </MainLayout>
       </MemoryRouter>,
+      rootElement,
     );
-    expect(component).toMatchSnapshot();
+    expect(container.firstChild).toMatchSnapshot();
   });
 });
